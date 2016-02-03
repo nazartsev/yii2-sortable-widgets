77298c5032ec9192c781ebd74c2737a17e3a1cd0
Nazartsev Egor <nazartsev@gmail.com>
2016-02-03 23:05:51 +0600

    fix sorted and js

--- assets/files/js/sortable-widgets.js
+++ assets/files/js/sortable-widgets.js
@@ -1,13 +1,13 @@
 function initSortableWidgets() {
-  $('[data-sortable-widget=1] tbody').sortable({
-    animation: 300,
-    handle: '.sortable-widget-handler',
-    dataIdAttr: 'data-key',
-    onEnd: function (e) {
-      $.post($(this.el).parents('[data-sortable-widget=1]').data('sortable-url'), {
-        sorting: this.toArray()
-      });
-    }
-  });
+	$('[data-sortable-widget=1] > table > tbody').sortable({
+		animation: 300,
+		handle: '.sortable-widget-handler',
+		dataIdAttr: 'data-id',
+		stop: function (evt, ui) {
+			var self = this;
+			$.post($(self).parents('[data-sortable-widget=1]').data('sortable-url'), {
+				sorting: $(self).sortable( "toArray", { attribute: "data-key" } )
+			});
+		}
+	});
 }
-
